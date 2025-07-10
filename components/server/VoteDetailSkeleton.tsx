7da72c1e6c05516391a949adbf8e8be44be98101
Charlie Hyun <ironlove77@gmail.com>
2025-07-10 19:00:13 +0900

    feat(VoteDetailSkeleton): 검색 필터 스켈레톤 추가 및 리워드 섹션 제거
    - 검색 필터 스켈레톤 UI 추가로 로딩 상태 시각화 개선
    - 리워드 섹션 스켈레톤 코드 제거로 코드 간결화 및 유지보수성 향상

--- components/server/VoteDetailSkeleton.tsx
+++ components/server/VoteDetailSkeleton.tsx
@@ -16,6 +16,12 @@ export default function VoteDetailSkeleton() {
         </div>
       </div>
 
+      {/* 검색 필터 스켈레톤 */}
+      <div className='mt-4 flex items-center gap-2 animate-pulse'>
+        <div className='h-10 bg-gray-200 rounded flex-1'></div>
+        <div className='h-10 w-24 bg-gray-200 rounded'></div>
+      </div>
+
       {/* 상위 3위 스켈레톤 */}
       <div className='sticky top-[72px] md:top-[96px] z-20 bg-white border-b'>
         <div className='flex gap-3 overflow-x-auto px-1 py-3 justify-center'>
@@ -32,30 +38,6 @@ export default function VoteDetailSkeleton() {
             </div>
           ))}
         </div>
-      </div>
-
-      {/* 리워드 섹션 스켈레톤 */}
-      <div className='mt-4 p-3 bg-gray-100 rounded-lg animate-pulse'>
-        <div className='h-6 bg-gray-200 rounded w-1/3 mb-3'></div>
-        <div className='flex gap-2 overflow-x-auto'>
-          {[...Array(3)].map((_, idx) => (
-            <div
-              key={idx}
-              className='flex items-center bg-white rounded-lg p-2 shadow-sm border border-gray-200 w-full max-w-[200px]'
-            >
-              <div className='w-10 h-10 rounded bg-gray-200 mr-2'></div>
-              <div className='flex-1'>
-                <div className='h-4 bg-gray-200 rounded w-4/5'></div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* 검색 필터 스켈레톤 */}
-      <div className='mt-4 flex items-center gap-2 animate-pulse'>
-        <div className='h-10 bg-gray-200 rounded flex-1'></div>
-        <div className='h-10 w-24 bg-gray-200 rounded'></div>
       </div>
 
       {/* 투표 항목 그리드 스켈레톤 */}
